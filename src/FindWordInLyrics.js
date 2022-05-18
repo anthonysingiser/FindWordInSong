a6d554fbcb91c6f6598b0ee0d925c10f30bea707
chrysanthemum <asingiser@gmail.com>
2022-05-18 14:36:35 -0400

    add toLowerCase method to main function

--- src/FindWordInLyrics.js
+++ src/FindWordInLyrics.js
@@ -2,21 +2,22 @@
 //and then counts how many times the 'soughtWord' occurs in the song
 
 export default function FindWordInLyrics(props){
-  console.log("Hello from FindWord")
   const punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
-    function RemovePunctuation() {
+  const lyricsWithNoPunctuation = RemovePunctuation()
+  
+  function RemovePunctuation() {
       return props.rawLyrics.split('')
       .filter((letter) => {
         return punctuation.indexOf(letter) === -1
         }).join('')
     }
-  const lyricsWithNoPunctuation = RemovePunctuation()
-  
+
   function main(){
     return lyricsWithNoPunctuation.split(' ').filter((word) =>{
-      return word === props.soughtWord
+      return word.toLowerCase() === props.soughtWord.toLowerCase()
     }).length
   }
+
   const numberOfOccurences = main();
   return( 
     <>
